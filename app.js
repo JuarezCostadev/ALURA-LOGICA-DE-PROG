ea53a8dce399ad01d902a15ffa32d87cd4808824
JuarezCosta <juarezcostasilva1999@outlook.com>
2025-01-29 21:05:14 -0300

    terceiro exercício em JS

--- app.js
+++
@@ -1,6 +0,0 @@
-let num = prompt('Informe um número: ')
-if(num >= 0){
-    alert(`O número ${num} é POSITIVO!`)
-}else{
-    alert(`O número ${num} é NEGATIVO!`)
-}
---
+++ img/Exercícios em JS/pontuaçao.js
@@ -0,0 +1,6 @@
+let pont = prompt('Insira sua pontuação no jogo: ');
+if(pont >= 100){
+    alert(`Sua pontuação foi ${pont}, VOCÊ VENCEU!`)
+}else{
+    alert(`Sua pontuação foi de ${pont}, TENTE NOVAMENTE!`)
+}
